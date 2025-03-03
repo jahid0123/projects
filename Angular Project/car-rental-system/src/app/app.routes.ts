ecfc0b13b961aaae346aa7c8eb2bc728b7f5a648
Jahid Ahmed <ajahid97@gmail.com>
2025-03-04 04:28:42 +0600

    updated

--- Angular Project/car-rental-system/src/app/app.routes.ts
+++ Angular Project/car-rental-system/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CarComponent } from './page/car/car.component';
+import { BookingComponent } from './page/booking/booking.component';
 
 export const routes: Routes = [
-    {path: 'add-car', component: CarComponent}
+    {path: 'add-car', component: CarComponent},
+    {path: 'booking', component: BookingComponent}
 ];
