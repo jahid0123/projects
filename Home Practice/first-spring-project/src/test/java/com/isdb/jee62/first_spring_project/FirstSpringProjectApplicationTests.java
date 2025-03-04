867476eca8e66c0bd23f55ec5a0701ca9e2301ca
Jahid Ahmed <ajahid97@gmail.com>
2025-03-04 23:04:18 +0600

    New project

---
+++ Home Practice/first-spring-project/src/test/java/com/isdb/jee62/first_spring_project/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isdb.jee62.first_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
